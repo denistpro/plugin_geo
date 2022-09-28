13723b1af72a5395bf9da9df5f0d349c62af2f52
denistpro <47992841+denistpro@users.noreply.github.com>
2022-09-28 09:37:26 +0300

    Update geo.php

--- geo/geo.php
+++ geo/geo.php
@@ -10,13 +10,5 @@ define( 'GEO__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 require_once (GEO__PLUGIN_DIR. '/includes/class-geo_activator.php');
 require_once (GEO__PLUGIN_DIR. '/includes/class-main_class.php');
 require_once (GEO__PLUGIN_DIR. '/includes/class-test.php');
-function call_mainClass() {
-	new mainClass();
-	
-}
-if ( is_admin() ) {
-	add_action( 'load-post.php', 'call_mainClass' );
-	add_action( 'load-post-new.php', 'call_mainClass' );
-}
 require_once (GEO__PLUGIN_DIR. '/shortcode/shortcode.php');
 ?>
