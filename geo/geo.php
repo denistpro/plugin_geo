38d813162fa0de83c1d2cadfa30b43e1d1e20719
denistpro <47992841+denistpro@users.noreply.github.com>
2022-09-28 13:32:17 +0300

    Add files via upload

--- geo/geo.php
+++ geo/geo.php
@@ -10,6 +10,14 @@ define( 'GEO__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 require_once (GEO__PLUGIN_DIR. '/includes/class-geo_activator.php');
 require_once (GEO__PLUGIN_DIR. '/includes/class-features.php');
 require_once (GEO__PLUGIN_DIR. '/includes/class-main_class.php');
+function call_mainClass() {
+	new mainClass();
+	
+}
+if ( is_admin() ) {
+	add_action( 'load-post.php', 'call_mainClass' );
+	add_action( 'load-post-new.php', 'call_mainClass' );
+}
 require_once (GEO__PLUGIN_DIR. '/includes/class-test.php');
 require_once (GEO__PLUGIN_DIR. '/shortcode/shortcode.php');
 ?>
