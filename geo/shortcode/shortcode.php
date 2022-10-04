33166fad91ef42670e0b081b936a4019d3538489
denistpro <47992841+denistpro@users.noreply.github.com>
2022-10-04 12:23:09 +0300

    Add files via upload

--- geo/shortcode/shortcode.php
+++ geo/shortcode/shortcode.php
@@ -35,21 +35,8 @@
 		// include views
 		require_once (GEO__PLUGIN_DIR. '/shortcode/shortcode-view.php');
 		
-		// include snippet-js for custom map
-		$strurl2 = '/js/shortcode-js.js';
-		wp_enqueue_script("shortcode-js", plugins_url($strurl2,__FILE__), null, null, true);
-		
-		// incluse google maps api key
-		if ($params["google-maps-api-key"])
-		{
-			$strurl = "https://maps.googleapis.com/maps/api/js?key=".$params['google-maps-api-key']."&callback=initMap";			
-		wp_enqueue_script('google-maps',$strurl,array('shortcode-js'),null,false);
-		
-		}
-		else
-		{
-			return ('Your Google API key is empty');
-		}
+		// inclede enqueues
+		require_once (GEO__PLUGIN_DIR. '/shortcode/shortcode-enqueues.php');
 		
 	wp_reset_postdata();
 	$output = ob_get_clean();	
